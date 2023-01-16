02e044d9cb82be6a18cc23ef3351d6ff6e3f3888
Michael van Rooijen <michael@vanrooijen.io>
2023-01-16 15:20:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Node Queue BullMQ (Autoscale.app)
+
+Produces [BullMQ] queue metrics for the [Autoscale.app] [Agent].
+
+## Installation
+
+Add the package to your `package.json`:
+
+    npm install @autoscale/queue-bullmq^0
+
+## Usage
+
+Instructions are provided during the autoscaler setup process on [Autoscale.app].
+
+## Development
+
+Prepare environment:
+
+    npm install
+
+See npm for relevant tasks:
+
+    npm run
+
+## Contributing
+
+Bug reports and pull requests are welcome on GitHub at https://github.com/autoscale-app/node-queue-bullmq
+
+[Autoscale.app]: https://autoscale.app
+[Agent]: https://github.com/autoscale-app/node-agent
+[BullMQ]: https://github.com/taskforcesh/bullmq
---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import { Queue, Backoffs, type BackoffStrategy, type Job } from 'bullmq'
+
+interface LatencyOptions {
+  lifo: boolean
+  connection: RedisConnectionOptions
+  backoffStrategy?: BackoffStrategy
+}
+
+interface RedisConnectionOptions {
+  host: string
+  port: number
+}
+
+const defaultLatencyOptions: LatencyOptions = {
+  lifo: false,
+  connection: {
+    host: '127.0.0.1',
+    port: 6379
+  }
+}
+
+export async function latency (names: string[], options: Partial<LatencyOptions> = {}): Promise<number | null> {
+  const opts: LatencyOptions = { ...defaultLatencyOptions, ...options }
+  let queues
+
+  try {
+    queues = names.map(queue => new Queue(queue, { connection: opts.connection }))
+    const requests = queues.map(async queue => await queue.getJobs(['wait'], 0, 0, !opts.lifo))
+    const responses = await Promise.all(requests)
+    const start = Date.now()
+    let oldest = start
+
+    for (const queue of responses) {
+      for (const job of queue) {
+        let jobStart
+
+        if (job.processedOn != null) {
+          // Unfortunately there doesn't appear to be a way to determine when the job failed,
+          // so the best we can do is use job.processedOn. It would be ideal if there was either
+          // a "job.failedOn" or "job.delayedUntil" property.
+          jobStart = job.processedOn + await backoffDelay(job, opts.backoffStrategy)
+        } else {
+          jobStart = job.timestamp + job.delay
+        }
+
+        if (jobStart < oldest) {
+          oldest = jobStart
+        }
+      }
+    }
+
+    return Math.round((start - oldest) / 1000)
+  } catch (err) {
+    console.error('[@autoscale/queue-bullmq] latency error:', err)
+    return null
+  } finally {
+    if (queues != null) {
+      for (const queue of queues) {
+        void queue.close()
+      }
+    }
+  }
+}
+
+async function backoffDelay (job: Job, backoffStrategy?: BackoffStrategy): Promise<number> {
+  if (job.opts.backoff == null) {
+    return 0
+  }
+
+  const backoffOptions = Backoffs.normalize(job.opts.backoff)
+  return await Backoffs.calculate(backoffOptions, job.attemptsMade, new Error(), job, backoffStrategy)
+}
