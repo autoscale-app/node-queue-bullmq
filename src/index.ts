d1202c3cffeeacff4c24e72eb5340e5875b3f65c
Michael van Rooijen <michael@vanrooijen.io>
2023-01-22 00:27:30 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,51 @@
-import { Queue } from "bullmq"
+import { Queue } from 'bullmq'
 
-type RedisConnectionOptions = {
-    host: string,
-    port: number
+interface LatencyOptions {
+  lifo: boolean
+  connection: RedisConnectionOptions
 }
 
-type LatencyOptions = {
-    lifo: boolean,
-    connection: RedisConnectionOptions
+interface RedisConnectionOptions {
+  host: string
+  port: number
 }
 
 const defaultLatencyOptions: LatencyOptions = {
-    lifo: false,
-    connection: {
-        host: "localhost",
-        port: 6379
-    }
+  lifo: false,
+  connection: {
+    host: 'localhost',
+    port: 6379
+  }
 }
 
 // @example
 //   latency(["default"])
 //   latency(["default", "low", "critical"], {lifo: true})
-export async function latency(names: string[], options: Partial<LatencyOptions> = {}) {
-    const opts: LatencyOptions = { ...defaultLatencyOptions, ...options }
-    const queues = names.map(queue => new Queue(queue))
-    const requests = queues.map(queue => queue.getJobs(["wait"], 0, 0, !opts.lifo))
+export async function latency (names: string[], options: Partial<LatencyOptions> = {}): Promise<number | null> {
+  const opts: LatencyOptions = { ...defaultLatencyOptions, ...options }
+  const queues = names.map(queue => new Queue(queue))
+  const requests = queues.map(async queue => await queue.getJobs(['wait'], 0, 0, !opts.lifo))
 
-    try {
-        const responses = await Promise.all(requests)
-        const start = Date.now()
-        let oldest = start
+  try {
+    const responses = await Promise.all(requests)
+    const start = Date.now()
+    let oldest = start
 
-        for (const queue of responses) {
-            for (const job of queue) {
-                if (job.timestamp < oldest) {
-                    oldest = job.timestamp
-                }
-            }
+    for (const queue of responses) {
+      for (const job of queue) {
+        if (job.timestamp < oldest) {
+          oldest = job.timestamp
         }
-
-        return Math.round((start - oldest) / 1000)
-    } catch (err) {
-        console.error("[@autoscale/queue-bullmq] latency error:", err)
-        return null
-    } finally {
-        for (const queue of queues) {
-            queue.close()
-        }
+      }
     }
+
+    return Math.round((start - oldest) / 1000)
+  } catch (err) {
+    console.error('[@autoscale/queue-bullmq] latency error:', err)
+    return null
+  } finally {
+    for (const queue of queues) {
+      void queue.close()
+    }
+  }
 }
