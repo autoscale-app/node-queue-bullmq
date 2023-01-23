09f35ed125d4cf552fa8a10904ce2679518fcb3e
Michael van Rooijen <michael@vanrooijen.io>
2023-01-23 13:01:59 +0100

    WIP

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,6 +4,18 @@ import { Queue } from "bullmq"
 
 let queue: Queue
 
+const OPTIONS = {
+  lifo: false,
+  connection: {
+    host: '127.0.0.1',
+    port: 6379
+  }
+}
+
+const LIFO_OPTIONS = Object.assign({}, OPTIONS, {
+  lifo: true
+})
+
 beforeAll(async () => {
   queue = new Queue("bullmq", {
     connection: {
@@ -23,26 +35,48 @@ afterAll(async () => {
 })
 
 test("empty", async () => {
-  await expect(latency(["bullmq"])).resolves.toBe(0)
+  await expect(latency(["bullmq"], OPTIONS)).resolves.toBe(0)
 })
 
 test("just enqueued", async () => {
   await queue.add("bullmq", {})
   await expect(queue.count()).resolves.toBe(1)
-  await expect(latency(["bullmq"])).resolves.toBe(0)
+  await expect(latency(["bullmq"], OPTIONS)).resolves.toBe(0)
 })
 
-test("enqueued 10 in the last second", async () => {
-  for (let i = 0; i < 10; i++) {
-    queue.add("bullmq", {})
+test("enqueued 6 in FIFO mode in the last 3 seconds", async () => {
+  for (let i = 0; i < 6; i++) {
+    setTimeout(() => {
+      queue.add("bullmq", {})
+    }, 500 * i)
   }
 
   return new Promise(async (resolve, reject) => {
-    await sleep(1000)
+    await sleep(3000)
 
     try {
-      await expect(queue.count()).resolves.toBe(10)
-      await expect(latency(["bullmq"])).resolves.toBe(1)
+      await expect(queue.count()).resolves.toBe(6)
+      await expect(latency(["bullmq"], OPTIONS)).resolves.toBe(3)
+      resolve(null)
+    } catch (err) {
+      reject(err)
+    }
+  })
+})
+
+test("enqueued 6 in LIFO mode in the last 3 seconds", async () => {
+  for (let i = 0; i < 6; i++) {
+    setTimeout(() => {
+      queue.add("bullmq", {})
+    }, 500 * i)
+  }
+
+  return new Promise(async (resolve, reject) => {
+    await sleep(3000)
+
+    try {
+      await expect(queue.count()).resolves.toBe(6)
+      await expect(latency(["bullmq"], LIFO_OPTIONS)).resolves.toBe(1)
       resolve(null)
     } catch (err) {
       reject(err)
@@ -52,5 +86,5 @@ test("enqueued 10 in the last second", async () => {
 
 test("null on error", async () => {
   jest.spyOn(Date, "now").mockImplementation(() => { throw new Error() })
-  await expect(latency(["bullmq"])).resolves.toBe(null)
+  await expect(latency(["bullmq"], OPTIONS)).resolves.toBe(null)
 })
